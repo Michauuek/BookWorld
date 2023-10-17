cae0fc1f893c1197ef2fc7f9e600a70be3c8a915
Michauuek <michal.rzemieniuk@gmail.com>
2023-10-17 21:58:53 +0200

    Added validation to test api

---
+++ prisma/migrations/20231017184714_added_lastname/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Authors" ADD COLUMN "lastName" TEXT;
+
+-- AlterTable
+ALTER TABLE "Users" ADD COLUMN "lastName" TEXT;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from 'cors';
+import {validationMiddleware} from "./service/validator";
+import {BookRequest} from "./model/bookDto";
+import {prisma} from "./utils/prisma";
+import {plainToInstance} from "class-transformer";
+import {GenreRequest} from "./model/genreDto";
 
 const app: Application = express();
 
@@ -9,6 +14,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Healthy");
+});
+
+
+// TODO move to controller
+// added only for test purposes
+app.post("/api/genre/create", validationMiddleware(GenreRequest), async (req: Request, res: Response) => {
+    const genre = plainToInstance(GenreRequest, req.body);
+    const savedGenre = await prisma.genres.create({
+        data: { name: genre.name }
+    });
+    res.status(201).json(savedGenre);
 });
 
 const PORT = process.env.PORT || 8000;
