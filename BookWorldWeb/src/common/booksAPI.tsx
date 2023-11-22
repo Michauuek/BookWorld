6d18d2f958a9abe59631de9387fd2b03da5f16e4
Michal Swistowski <miswistowski@gmail.com>
2023-11-22 23:36:20 +0100

    some changes

--- BookWorldWeb/src/common/booksAPI.tsx
+++ BookWorldWeb/src/common/booksAPI.tsx
@@ -6,6 +6,39 @@ export type RatingRequest = {
     rating: number
 }
 
+export type BookRatingResponse = {
+    id: number
+    bookId: number
+    userId: string
+    comment?: string
+    rating: number
+}
+
 export const AddRating = (request: RatingRequest) => {
     return axios.post('/api/ratings/create', request)
+}
+
+export const GetRating = (bookId: number, userId: string) => {
+    const payload = {
+        where: {
+          AND: [
+            {
+              bookId: {
+                equals: bookId
+              }
+            },
+            {
+                userId: {
+                  equals: userId
+                }
+              },
+          ]
+        },
+        pagination: {
+          take: 1,
+          skip: 0
+        },
+      }
+      console.log(`requesting rating for book ${bookId} and user ${userId}`)
+    return axios.post(`/api/ratings/elastic/get`, payload)
 }
