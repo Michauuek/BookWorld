738d1eb6644ed1e9b71e05262138dc1ecee2abe4
Karolina <karolinajez21@gmail.com>
2023-11-25 20:51:26 +0100

    Add toasts on login and register events

--- BookWorldWeb/src/main.tsx
+++ BookWorldWeb/src/main.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-import { AuthProvider } from './common/auth.tsx'
+import { AuthProvider } from './common/auth'
+import { ToastContainer, Flip } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
+      <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          closeOnClick={false}
+          pauseOnFocusLoss={false}
+          draggable={true}
+          pauseOnHover={true}
+          transition={Flip}
+      />
   </React.StrictMode>,
 )
